947c8cac0c65449c20375c4049acaf125963d991
Camm1281 <camilaburguete632@gmail.com>
2025-05-08 14:37:17 -0600

    si

--- Prueba.py
+++ Prueba.py
@@ -4,8 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-<<<<<<< HEAD
-    return "<p>Holaaa, World!</p>"
-=======
+
     return "<p>Hello, World Mdfkers!</p>"
->>>>>>> 19a49df384a7d41bd14f93ea0b56b63811e23d8d
